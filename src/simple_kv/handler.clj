d3e94a692710c5ea8b91fcdf4ca311c6dfe57d54
Rirush <RaccoonRirush@gmail.com>
2021-08-26 08:57:58 +0300

    fix: make code a bit more pretty

--- src/simple_kv/handler.clj
+++ src/simple_kv/handler.clj
@@ -2,7 +2,7 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]])
-  (:import (java.io InputStream InputStreamReader)))
+  (:import (java.io InputStream)))
 
 (def state (atom {}))
 
@@ -14,12 +14,14 @@
                (swap! state assoc key value)
                {:status 200
                 :body   (str "SET " key " TO " value " OK")}))
+
            (GET "/get/:key" req
              (let [key (-> req :params :key)
                    cont (contains? @state key)]
                (if cont
                  {:status 200 :body (get @state key)}
                  {:status 404 :body "NIL"})))
+
            (DELETE "/del/:key" req
              (let [key (-> req :params :key)
                    cont (contains? @state key)]
@@ -28,15 +30,18 @@
                      {:status 200
                       :body   (str "DEL " key " OK")})
                  {:status 404 :body "NIL"})))
+
            (GET "/len" []
              {:status 200
               :body   (str (count @state))})
+
            (GET "/find" [q]
              (let [r (re-pattern q)]
                {:status 200
                 :body   (map #(str "KEY " (first %) " VALUE " (second %) "\n")
                              (into {}
                                    (filter #(re-matches r (first %)) @state)))}))
+
            (GET "/" [] "very awesome kv server")
            (route/not-found "Not Found"))
 
